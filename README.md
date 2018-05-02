3f3808bfbe5fae430d1e8b0846b3902231bd3b12
Marcus Renno <renno.marcus@gmail.com>
2018-05-02 09:02:41 -0400

    Created example with sprite usage

---
+++ README.md
@@ -0,0 +1,13 @@
+# Examples for Macaw
+
+All the examples here use [macaw](https://github.com/tubelz/macaw/), a 2D Game Engine using SDL2 written in Go.
+
+## Installation and requirements
+* SDL2:
+	You will need to install SDL2 in your machine and the binding for Go.
+	You can find more information here: [https://github.com/veandco/go-sdl2]
+* `go get -v github.com/tubelz/macaw`
+* To run an example `go run folder\file.go`, where folder and file are within this project
+
+## License
+The code here is under the GNU v2 license. You can read more [here](https://github.com/tubelz/examples/LICENSE.txt)
---
+++ sprite/sprite.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"github.com/tubelz/macaw"
+	"github.com/tubelz/macaw/entity"
+	"github.com/tubelz/macaw/input"
+	"github.com/tubelz/macaw/system"
+	"github.com/veandco/go-sdl2/sdl"
+)
+
+func main() {
+	var err error
+	// Initialize SDL_IMG. We don't need to initialize TTF and MIX
+	err = macaw.Initialize(true, false, false)
+	if err != nil {
+		fmt.Println("Macaw could not initialize")
+	}
+	defer macaw.Quit()
+
+  input := &input.Manager{}
+	em := &entity.Manager{}
+  // render is the only system we need here
+	render := &system.RenderSystem{Name: "render system", Window: macaw.Window, EntityManager: em}
+	render.Init()
+	initializeEntities(em, render)
+
+	render.SetCamera(em.Get(1))
+	gameLoop := initializeGameLoop(render, input)
+	gameLoop.Run()
+}
+
+// initializeEntities will create the sprite and camera
+func initializeEntities(em *entity.Manager, render *system.RenderSystem) {
+	objSpritesheet := &entity.Spritesheet{}
+	objSpritesheet.Init(render.Renderer, "assets/macaw.png")
+	// Load sprites from spritesheet
+	macawCrop := &sdl.Rect{0, 0, 200, 200}
+	sprite := objSpritesheet.LoadSprite(macawCrop)
+	obj := em.Create("macaw")
+	obj.AddComponent("render", &sprite)
+	obj.AddComponent("position", &entity.PositionComponent{&sdl.Point{100, 100}})
+
+	camera := em.Create("camera")
+	camera.AddComponent("position", &entity.PositionComponent{&sdl.Point{0, 0}})
+	camera.AddComponent("camera", &entity.CameraComponent{
+		ViewportSize: sdl.Point{800, 600},
+		WorldSize:    sdl.Point{1145, 600},
+	})
+}
+
+func initializeGameLoop(render *system.RenderSystem, im *input.Manager) *macaw.GameLoop {
+	gameLoop := &macaw.GameLoop{InputManager: im}
+	sceneGame := &macaw.Scene{Name: "example"}
+	sceneGame.AddRenderSystem(render)
+	gameLoop.AddScene(sceneGame)
+
+	return gameLoop
+}
